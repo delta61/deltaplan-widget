5ffdc8601b259e8a88b296341550f5b981973e04
Vanik <asdf3107@mail.ru>
2021-05-14 17:39:58 +0300

    index

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Личный кабинет Дельтаплан</title>
+  
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/fontawesome.css"
+        integrity="sha384-HbmWTHay9psM8qyzEKPc8odH4DsOuzdejtnr+OFtDmOcIVnhgReQ4GZBH7uwcjf6" crossorigin="anonymous">
+
+</head>
+<body>
 
 
+    <?php
 
-// tratata
+        echo '34322';
+    ?>
 
-
-echo "21342134-23324-***********===";
-
-
-
+    <script>
 
 
-sdfsdfsdfsdf
-sdfsdfsdfsdfs
-df
+    </script>
+
+</body>
+</html>
