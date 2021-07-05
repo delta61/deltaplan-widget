4bf93ea0448e3bfe4c4508c7b640f7ba0b3392f7
Vanik <asdf3107@mail.ru>
2021-07-05 20:20:27 +0300

    db

---
+++ inc/access.php
@@ -0,0 +1,30 @@
+<?php
+    defined('INCLUDED') or die('not included');
+
+
+
+    
+    if (
+            && isset($_SERVER['PHP_AUTH_USER'])
+            && $_SERVER['PHP_AUTH_USER']
+            && strpos($_SERVER['PHP_AUTH_USER'],' ') === false
+        ) {
+            $auth_user = $db->escape(strtolower($_SERVER['PHP_AUTH_USER']));
+            $auth_pw = $db->escape($_SERVER['PHP_AUTH_PW']);
+            $res = $db->query("SELECT * FROM workers
+                WHERE login='{$auth_user}'
+                    AND (pass='{$auth_pw}' OR pass='')
+                    AND `post`<>'hide'
+                    AND enbl='y'
+                LIMIT 1");
+            if ( ! $res) exit('db error');
+        }
+        if ( ! $res || ! $db->num_rows($res)) {
+                header('WWW-Authenticate: Basic realm="Admin Page"');
+                header('HTTP/1.0 401 Unauthorized');
+                exit();
+            
+        }
+
+        unset($auth_pw);
+        $_userinfo = $db->fetch_assoc($res);
